3a870bf0bcf87d8e92341e34c8e6592e1baebafa
aditya <adityahsahrawat985@gmail.com>
2024-06-07 00:28:07 +0530

    made modal responsive

--- app/components/Tasks/DeleteAllTasksModal.jsx
+++ app/components/Tasks/DeleteAllTasksModal.jsx
@@ -56,7 +56,7 @@ export default function DeleteAllTasksModal({ getUserTasks }) {
           </TransitionChild>
 
           <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
               <TransitionChild
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
@@ -89,7 +89,7 @@ export default function DeleteAllTasksModal({ getUserTasks }) {
                       </div>
                     </div>
                   </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <div className="bg-gray-50 px-4 pb-3 flex gap-3 items-center sm:px-6 justify-end">
                     <button
                       type="button"
                       disabled={loading}
@@ -106,7 +106,7 @@ export default function DeleteAllTasksModal({ getUserTasks }) {
                     </button>
                     <button
                       type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      className="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                       onClick={() => setOpen(false)}
                       data-autofocus
                     >
